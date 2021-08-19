2feb34b5e898963f6c6c605e6d699aad3a73521b
gabrielblins <academic.gabrielblins@gmail.com>
2021-08-19 04:21:23 -0300

    update graphs

--- data_view.py
+++ data_view.py
@@ -31,3 +31,33 @@ fig4 = px.histogram(loan_df, x="annual.inc", color="not.fully.paid",
                     hover_data=loan_df.columns,
                     title='Histograma e Violin plot do Annual Income em relação a not.fully.paid')
 
+fig5 = px.histogram(loan_df, x="installment", color="not.fully.paid",
+                    marginal="violin", # or violin, rug
+                    hover_data=loan_df.columns,
+                    title='Histograma e Violin plot do Installment em relação a not.fully.paid')
+
+fig6 = px.scatter_3d(loan_df, x='int.rate', y='revol.util', z='fico',
+                     color='not.fully.paid', 
+                     labels={'int.rate':'Interest Rate', 'revol.util': '% Usada de crédito', 'days.with.cr.line': 'FICO Score'})
+
+
+markdown_1 = '''
+---
+## Columns Info
+* **credit.policy**: 1 se o usuário se encontra sobre os critérios de crédito do LendingClub.com, e 0 caso ao contrário.
+* **purpose**: O propósito do empréstimo (recebe valores como "creditcard", "debtconsolidation", "educational", "majorpurchase", "smallbusiness", and "all_other").
+* **int.rate**: Juros do empréstimo, como uma proporção (se a taxa de juros for 11% será registrada como 0.11).
+* **installment**: As prestações mensais que o beneficiado deve se o empréstimo for financiado.
+* **log.annual.inc**: O log natural da receita anual declarada pelo usuário(quem requisitou o empréstimo).
+* **annual.inc**: receita anual declarada pelo usuário(quem requisitou o empréstimo).
+* **dti**: A razão de débito do beneficiado (total de débito dividido pela receita anual).
+* **fico**: A pontuação FICO do beneficiado.
+* **days.with.cr.line**: O número de dias que o beneficiado teve sobre a linha de crédito.
+* **revol.bal**: O saldo rotativo do beneficiado (o valor não pago do cartão de crédito ao final do ciclo de faturamento).
+* **revol.util**: A taxa de utilização da linha rotativa do mutuário (o valor da linha de crédito usado em relação ao crédito total disponível).
+* **inq.last.6mths**: O número de consultas do mutuário por credores nos últimos 6 meses.
+* **delinq.2yrs**: O número de vezes que o beneficiário ficou sem pagar após 30+ dias do pagamento em 2 anos.
+* **pub.rec**: O número de depreciações registradas(pedidos de falência, gravames fiscal, or processos/julgamentos). 
+* **not.fully.paid**: 0 se o benficiado pagou todo seu débito, 1 se não foi totalmente pago.
+'''
+
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from data_view import fig1, fig2, loan_df, fig3, fig4
+from data_view import fig1, fig2, loan_df, fig3, fig4, fig5, fig6, markdown_1
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -8,23 +8,35 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title('DashBoard de análise de empréstimos')
 st.write('Esse prejeto consiste na analise de dados relativo à empréstimos no qual tentamos visualizar relações entre features que possuimos')
 
-st.sidebar.title("Configuração")
-check_box = st.sidebar.checkbox(label="mostrar database")
+st.sidebar.title("Configurações")
+check_box = st.sidebar.checkbox(label="Mostrar database")
+check_box_col = st.sidebar.checkbox(label="Descrição de colunas")
 
 if check_box:
     st.write(loan_df)
+
+st.markdown("---")
+st.markdown("## Descrição")
+if check_box_col:
+    st.markdown(markdown_1)
+
+
+st.markdown("---")
+st.markdown("## Visualização dos dados")
 
 st.plotly_chart(fig1)
 st.plotly_chart(fig2)
 st.plotly_chart(fig3)
 st.plotly_chart(fig4)
+st.plotly_chart(fig5)
+st.plotly_chart(fig6)
 
-plt.figure(figsize=(10,8))
-sns.histplot(data=loan_df,
-                    x="annual.inc", 
-                    element='step', 
-                    fill=True, 
-                    hue="not.fully.paid")
-plt.xlim([-10000, 0.4e6])
-plt.grid()
-st.pyplot()
+# plt.figure(figsize=(10,8))
+# sns.histplot(data=loan_df,
+#                     x="annual.inc", 
+#                     element='step', 
+#                     fill=True, 
+#                     hue="not.fully.paid")
+# plt.xlim([-10000, 0.4e6])
+# plt.grid()
+# st.pyplot()
